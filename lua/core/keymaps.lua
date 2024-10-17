c403313d666c45fbdb248cc514d9b1d38d6bcd77
LingengMa <1370120724@qq.com>
2024-10-17 21:28:41 +0800

    feat: terminal shortcut

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,3 +13,11 @@ vim.api.nvim_set_keymap('v', '<F1>', '<Nop>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap=true, silent=true })
 vim.api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', { noremap=true, silent=true })
+
+-- 设置 leader 键为空格
+vim.g.mapleader = ";"
+
+-- 终端模式下按ESC返回正常模式
+vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+-- 设置 leader + j 快捷键，向下分割窗口并打开终端
+vim.api.nvim_set_keymap('n', '<leader>j', ':split<CR><C-w>j:terminal<CR>i', { noremap = true, silent = true })
