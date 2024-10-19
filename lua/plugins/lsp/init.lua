84495b2455bf29c7809099440f9001c289cdff61
LingengMa <1370120724@qq.com>
2024-10-19 17:06:31 +0800

    feat: 添加Rust Lsp

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -2,20 +2,11 @@ local lspconfig = require("lspconfig")
 
 require('null-ls')
 
--- 启用 clangd 作为 C/C++ 的 LSP
 require("mason").setup()
 require("mason-lspconfig").setup {
   ensure_installed = { "clangd" }
 }
 
--- 配置 clangd
-lspconfig.clangd.setup {
-  on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-  end,
-  flags = {
-    debounce_text_changes = 150,
-  }
-}
 
+require('plugins.lsp.c_config')
+require('plugins.lsp.rust_config')
---
+++ lua/plugins/lsp/rust_config.lua
@@ -0,0 +1,23 @@
+local lspconfig = require("lspconfig")
+
+-- 启用 rust-analyzer 作为 Rust 的 LSP
+lspconfig.rust_analyzer.setup {
+  on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  end,
+  flags = {
+    debounce_text_changes = 150,
+  },
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+      checkOnSave = {
+        command = "clippy",
+      },
+    }
+  }
+}
+
