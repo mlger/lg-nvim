a9412093aba496281918c7feecd75c33c39ebcf3
LingengMa <1370120724@qq.com>
2024-10-23 22:03:33 +0800

    feat: 魔改主题

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,5 +12,18 @@ vim.opt.tabstop = 2        -- 一个 Tab 等于 2 个空格
 vim.opt.shiftwidth = 2     -- 缩进级别为 2 个空格
 vim.opt.expandtab = true   -- 用空格代替 Tab
 
--- 设置主题
+-- -- 设置主题
 vim.cmd('colorscheme github_dark')
+
+-- 设置 Floaterm 的背景和前景色，使用 Normal 的颜色
+vim.cmd('highlight Floaterm guibg=none guifg=#E6EDF3')
+
+-- 设置 FloatermBorder 的背景色，使用 NormalFloat 的背景色，前景色使用 Normal 的前景色
+vim.cmd('highlight FloatermBorder guibg=#30363d guifg=none')
+
+-- 设置 FloatermNC 的背景和前景色，使用 NormalNC 的颜色
+vim.cmd('highlight FloatermNC guibg=red guifg=green')
+
+-- 设置 NvimTree 的背景和前景色，使用 Normal 的颜色
+vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#30363d", fg = "none" })
+
